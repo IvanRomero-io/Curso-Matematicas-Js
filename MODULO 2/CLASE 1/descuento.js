a8672b76b0d107950c8efd8eae476e3016838aad
IvanRomero-io <ivandejesusrl2001@gmail.com>
2023-01-02 09:36:16 -0500

    arreglo bug de descuento

--- MODULO 2/CLASE 1/descuento.js
+++ MODULO 2/CLASE 1/descuento.js
@@ -1,13 +1,15 @@
-
+//AGARRE LOS ELEMENTOS EN EL HTML
 const precioInput = document.querySelector('.precio')
 const descuentoInput = document.querySelector('.descuento')
 const botonCalcular = document.querySelector('.boton-resultado') 
 const pResultado = document.querySelector('.p-resultado')
 const botonReinicio = document.querySelector('.boton-refrescar')
 
+//LE DI LISTENER A ALGUNOS ELEMENTOS.
 botonCalcular.addEventListener('click', accionBoton)
 botonReinicio.addEventListener('click', reiniciarPagina)
 
+//DECLARE VARIABLES 
 let precio;
 let descuento;
 let compra;
@@ -19,8 +21,8 @@ function accionBoton(){
 
     compra = (precio * (100 - descuento)) / 100
 
-    if( precioInput.value == "" || descuentoInput.value == "" || precio < 0 || descuento < 0 ){
-        pResultado.innerHTML = "No Ingresaste numeros o usaste numeros negativos... intentalo denuevo"
+    if( precioInput.value == "" || descuentoInput.value == "" || precio < 0 || descuento < 0 || descuento > 100){
+        pResultado.innerHTML = "No Ingresaste numeros, usaste numeros negativos o usaste mas de 100% de descuento... intentalo denuevo"
     } else {
         console.log(compra)
         pResultado.innerHTML = "El descuento es de: " + (precio - compra) + "$ <br> El monto total a pagar es de: " + compra + "$" 
