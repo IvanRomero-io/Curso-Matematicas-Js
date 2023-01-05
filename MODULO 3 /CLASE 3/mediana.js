03d8692695b207341422d7b8b528730f46110cb4
IvanRomero-io <ivandejesusrl2001@gmail.com>
2023-01-05 13:26:28 -0500

    Buscar mediana

--- MODULO 3 /CLASE 3/mediana.js
+++ MODULO 3 /CLASE 3/mediana.js
@@ -10,12 +10,17 @@
     const listaEsPar = esPar(arr)
 
     if(listaEsPar){
-        console.log('par')
-    }
-    else{
+        const posicion1  = arr[arr.length / 2 - 1]
+        const posicion2 = arr[arr.length / 2]
+        const mitadArray = [posicion1, posicion2]
+        console.log(mitadArray)
+        buscarElPromedio(mitadArray)
+        
+      }
+    else{ 
       const indexMitadListaImpar = Math.round(arr.length / 2) - 1
       const medianaListaImpar = arr[indexMitadListaImpar]
-      console.log(indexMitadListaImpar + " <-- posicion del array meitad en la lista")
+      console.log(indexMitadListaImpar + " <-- posicion del array mitad en la lista")
       console.log(medianaListaImpar + " <-- array el cual es la mitad")
       return medianaListaImpar
       
@@ -34,9 +39,7 @@ function buscarElPromedio(arr){
 
 
 
-function esPar(lista){
-    return !(lista.length % 2)
-}
+
 
 
 
